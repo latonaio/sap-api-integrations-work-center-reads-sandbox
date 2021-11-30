490320195e4cbc5b84b852d8feed0ee90bf5e1fd
Yurino Oga <yurino.o@latona.io>
2021-11-30 14:04:50 +0900

    release

--- SAP_API_Output_Formatter/type.go
+++ SAP_API_Output_Formatter/type.go
@@ -1,4 +1,4 @@
-package sap_api_caller
+package sap_api_output_formatter
 
 type WorkCenterReads struct {
 	 ConnectionKey    string `json:"connection_key"`
@@ -7,11 +7,11 @@ type WorkCenterReads struct {
 	 Filepath         string `json:"filepath"`
 	 APISchema        string `json:"api_schema"`
 	 WorkCenter       string `json:"work_center_code"`
-	 Deleted          string `json:"deleted"`
+	 Deleted          bool   `json:"deleted"`
 }
 
 type WorkCenter struct {
-     WorkCenterInternalID         int    `json:"WorkCenterInternalID"`
+     WorkCenterInternalID         string `json:"WorkCenterInternalID"`
      WorkCenterTypeCode           string `json:"WorkCenterTypeCode"`
      WorkCenter                   string `json:"WorkCenter"`
      WorkCenterDesc               string `json:"WorkCenter_desc"`
@@ -20,11 +20,11 @@ type WorkCenter struct {
      WorkCenterResponsible        string `json:"WorkCenterResponsible"`
      SupplyArea                   string `json:"SupplyArea"`
      WorkCenterUsage              string `json:"WorkCenterUsage"`
-     MatlCompIsMarkedForBackflush string `json:"MatlCompIsMarkedForBackflush"`
+     MatlCompIsMarkedForBackflush bool   `json:"MatlCompIsMarkedForBackflush"`
      WorkCenterLocation           string `json:"WorkCenterLocation"`
-     CapacityInternalID           int    `json:"CapacityInternalID"`
+     CapacityInternalID           string `json:"CapacityInternalID"`
      CapacityCategoryCode         string `json:"CapacityCategoryCode"`
      ValidityStartDate            string `json:"ValidityStartDate"`
      ValidityEndDate              string `json:"ValidityEndDate"`
-     WorkCenterIsToBeDeleted      string `json:"WorkCenterIsToBeDeleted"`
+     WorkCenterIsToBeDeleted      bool   `json:"WorkCenterIsToBeDeleted"`
 }
